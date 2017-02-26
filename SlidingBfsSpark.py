f3ddbeb1eac5f5e9482354e6e2ce0f8755cf1de8
neoluc <neoluc@hotmail.com>
2017-02-26 14:51:53 +0800

    updated some codes

--- SlidingBfsSpark.py
+++ SlidingBfsSpark.py
@@ -3,27 +3,37 @@ import argparse
 import Sliding
 
 def solve_puzzle(width, height, output_path, slave_number):
+	def hash_to_board(state):
+		return Sliding.hash_to_board(width, height, state)
+	def board_to_hash(board):
+		return Sliding.board_to_hash(width, height, board)
+	def get_children_boards(board):
+		return Sliding.children(width, height, board)
+	def get_solution_hash():
+		return Sliding.board_to_hash(width, height, Sliding.solution(width, height))
+
 	sc = SparkContext("local", "Slide")
-	hash_solution = Sliding.board_to_hash(width, height, Sliding.solution(width, height))
-	boards_rdd = sc.parallelize([(hash_solution, 0)])
-	level = 0
+	boards_rdd = sc.parallelize([(get_solution_hash(), 0)])
+	current_level = 0
 	while True:
-		level += 1
-		frontier_rdd = boards_rdd.filter(lambda step: step[1] == level - 1)
+		current_level += 1
+		frontier_rdd = boards_rdd.filter(lambda (state, level): level == current_level - 1)
+		frontier_rdd.persist()
 		if frontier_rdd.isEmpty():
 			break
 		boards_rdd = frontier_rdd\
-			.flatMap(lambda step: Sliding.children(width, height, Sliding.hash_to_board(width, height, step[0])))\
-			.map(lambda board_hash: (Sliding.board_to_hash(width, height, board_hash), level))\
+			.flatMap(lambda (state, level): get_children_boards(hash_to_board(state)))\
+			.map(lambda state_board: (get_children_boards(state_board), current_level))\
 			.union(boards_rdd)\
 			.reduceByKey(lambda step_level_a, step_level_b: min(step_level_a, step_level_b))\
 			.partitionBy(slave_number)
 
 	boards_rdd\
-		.map(lambda step: (step[1], Sliding.hash_to_board(width, height, step[0])))\
+		.map(lambda (state, level): (level, hash_to_board(state)))\
 		.sortByKey()\
 		.coalesce(1)\
 		.saveAsTextFile(output_path)
+
 	sc.stop()
 
 if __name__ == "__main__":
